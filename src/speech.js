25549a5d38c520e7f7c10d9c054977bcc85e0d8a
tedere93 <tedere87@gmail.com>
2017-05-05 19:54:24 +0300

    SpeechRecognition works flawless xD

---
+++ src/speech.js
@@ -0,0 +1,26 @@
+window.addEventListener("touchstart",ontouchstart);
+window.addEventListener("mousedown",ontouchstart);
+
+
+var recog = new webkitSpeechRecognition();
+
+
+
+function ontouchstart(){
+	recog.start();
+
+
+
+}
+
+function onEnd(){
+	recog.stop();
+}
+
+recog.onend = onEnd();
+
+recog.onsoundend = onEnd();
+
+recog.onresult = function (e){
+	document.getElementById("text").innerHTML = e.results[0][0].transcript;
+}
